10d6cad50eebe4aa65e77d37a64718eea5780738
IvanBustos <chinobustos5@gmail.com>
2023-11-24 13:05:57 -0300

    haciendo la ultima entrega

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
 const contenedorProductos = [
   {
     id: 1,
     nombre: "Café Capuccino",
     precio: 1800,
-    img: "file:///C:/Users/Usuario/Desktop/chino/proyecto%20java/multimedia/Cappuccino-1.png",
+    img: "../multimedia/capuccino.jpg",
   },
   {
     id: 2,
@@ -37,7 +40,7 @@ const contenedorProductos = [
   },
   {
     id: 7,
-    nombre: "Café con Leche<",
+    nombre: "Café con Leche",
     precio: 1600,
     img: "../multimedia/cafe con leche.jpg",
   },
@@ -72,12 +75,46 @@ const contenedorProductos = [
     img: "../multimedia/Licuados.webp",
   },
 ];
-contenedorProductos.forEach((item) => {
+let carrito= [];
+contenedorProductos.forEach((product) => {
   let content = document.createElement("div");
+  content.className = "card"
   content.innerHTML = `
     <img src="${product.img}">
     <h3> ${product.nombre}<h3>
-    <p>${product.precio}$<p>
+    <p class= "price">${product.precio}$<p>
     `;
-  contenedorProductos = append(content);
+  shopContent.append(content);
+
+   let comprar = document.createElement("button")
+   comprar.innerText = "comprar";
+   comprar.innerHTML = "comprar"
+
+   content.append(comprar);
+
+   comprar.addEventListener("click", () =>{
+    carrito.push({
+      id : product.id,
+      img: product.img,
+      nombre: product.nombre,
+      precio: product.precio,
+    });
+    console.log(carrito);
+   });
+});
+verCarrito.addEventListener("click" , () =>{
+  const modalHeader = document.createElement("div");
+  modalHeader.className = "modalHeader"
+  modalHeader.innerHTML = `
+  <h1 class = "modal-heade-title">carrito</h1>
+  `;
+  modalContainer.append(modalHeader);
+
+  const modalbutton = document.createElement("h1");
+  modalbutton.innerText = "x";
+  modalbutton.className = "modal-header-button";
+
+  modalHeader.append(modalbutton);
+
+  
 });
